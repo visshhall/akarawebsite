03b4671ade5e0865e106041fca291988220663a6
Vishal <214860295+visshhall@users.noreply.github.com>
2026-09-05 19:39:34 +0530

    Add files via upload

---
+++ public/gtag-init.js
@@ -0,0 +1,7 @@
+/* ĀKĀRA analytics bootstrap — external file so CSP script-src can omit 'unsafe-inline' */
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag("js", new Date());
+gtag("config", "G-TBCH7JXTE9");
+gtag("config", "GT-TQS95GN4");
+gtag("config", "GT-5MXXFXXN");
