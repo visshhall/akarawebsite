cdbb4b1ca4062dcfa2e2a856c043a0c167860fce
Vishal <214860295+visshhall@users.noreply.github.com>
2026-09-14 13:25:48 +0530

    up

--- marketing_columns.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE customers ADD COLUMN IF NOT EXISTS marketing_email_opt_in BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE customers ADD COLUMN IF NOT EXISTS marketing_whatsapp_opt_in BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE customers ADD COLUMN IF NOT EXISTS abandoned_cart_reminder_sent_at TIMESTAMPTZ;
-ALTER TABLE orders ADD COLUMN IF NOT EXISTS care_email_sent_at TIMESTAMPTZ;
